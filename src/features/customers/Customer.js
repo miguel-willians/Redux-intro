1b4c2eac66dc539d1e30412b0d7cfcd3c49f3868
miguel-willians <miiguel.wso@gmail.com>
2024-12-16 16:41:57 -0300

    Provendo o estado para Customer

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,5 +1,9 @@
+import { useSelector } from "react-redux";
+
 function Customer() {
-  return <h2>👋 Welcome, %NAME%</h2>;
+  const customer = useSelector((store) => store.customer.fullName); //O useSelector possibilita o consumo do estado da store
+
+  return <h2>👋 Welcome, {customer}</h2>;
 }
 
 export default Customer;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// Para conectar o react com o redux, é necessárion instalar o pacote react-redux com o npm. O funcionamento do Provider é semelhante ao provider da Context API
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 
@@ -11,6 +13,8 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
