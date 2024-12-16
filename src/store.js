05d08a4ef66a702a43f07a919a747f135753b225
miguel-willians <miiguel.wso@gmail.com>
2024-12-16 12:29:48 -0300

    Modificando a estrutura de arquivos

--- src/store.js
+++ src/store.js
@@ -1,123 +1,13 @@
 import { combineReducers, createStore } from "redux";
 
-// O redux nada mais é que um gerenciador de estado global. Os estados são gerenciados através da store, um conjunto de reducers que executarão as atualizações.
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
 
-// createStore é uma função obsoleta que foi substituída no RTK
-
-const initialStateAccount = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: "",
-};
-
-const initialStateCustomer = {
-  fullName: "",
-  nationalID: "",
-  createdAt: "",
-};
-
-function accountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-    default:
-      return state;
-  }
-}
-
-function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: action.payload.createdAt,
-      };
-    case "customer/updateName":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-      };
-    default:
-      return state;
-  }
-}
-// Root reducer: O reducer que combinará todos os outros
 const rootReducers = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
 const store = createStore(rootReducers);
-// store.dispatch({ type: "account/deposit", payload: 50 });
 
-// console.log("teste");
-
-// store.dispatch({
-//   type: "account/requestLoan",
-//   payload: { amount: 10000, purpose: "Buy a car" },
-// });
-
-// Action creators: funções que retornam ações. Não é algo do Redux, mas é utilizado por convenção
-
-function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
-}
-
-function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-
-function requestLoan(amount, purpose) {
-  return {
-    type: "account/requestLoan",
-    payload: { amount, purpose },
-  };
-}
-
-function payLoan() {
-  return { type: "account/payLoan" };
-}
-
-store.dispatch(deposit(500));
-console.log(store.getState());
-store.dispatch(withdraw(200));
-
-store.dispatch(requestLoan(10000, "Buy a car"));
-console.log(store.getState());
-
-store.dispatch(payLoan());
-console.log(store.getState());
-
-function createCustomer(fullName, nationalID) {
-  return {
-    type: "customer/createCustomer",
-    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
-  };
-}
-
-function updateName(fullName) {
-  return { type: "account/updateName", payload: fullName };
-}
-
-store.dispatch(createCustomer("Miguel Willians", 1144845));
-console.log(store.getState());
+export default store;
