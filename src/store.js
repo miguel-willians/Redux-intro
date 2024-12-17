b7c269e8691d4f680e8c68361c123ba22ce9d51f
miguel-willians <miiguel.wso@gmail.com>
2024-12-17 14:08:35 -0300

    Criando o estado isLoading

--- src/store.js
+++ src/store.js
@@ -1,13 +1,17 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+
+// Redux thunk: Permite a utilização de ações assíncronas atualizando o estado antes do dispatch chegar na store.
+import { thunk } from "redux-thunk";
 
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
-const rootReducers = combineReducers({
+const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducers);
+// As ações assíncronas ocorrem no Middleware:
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
